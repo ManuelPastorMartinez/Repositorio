b69a9ddb54c1457f57697a595c2b08711d54933d
A714\dam1 <manuelpastorx243@gmail.com>
2025-09-26 19:33:13 +0200

    Baterias

--- Ejercicios_1DAM/src/main/java/org/example/Bateria3.java
+++ Ejercicios_1DAM/src/main/java/org/example/Bateria3.java
@@ -1,4 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Bateria3 {
+    static void main(){
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Dime un número");
+        float numero1 = teclado.nextFloat();
+        if (numero1>0){
+            System.out.println(numero1 +" es mayor que 0");
+        } else if (numero1<0) {
+            System.out.println(numero1 +" es menor que 0");
+        } else {
+            System.out.println(numero1 +" es 0");
+        }
+    }
+
 }
---
+++ Ejercicios_1DAM/src/main/java/org/example/bateria4.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class bateria4 {
+    static void main(){
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Dame un número");
+        int numero1 = teclado.nextInt();
+
+        System.out.println("Dame otro número");
+        int numero2 = teclado.nextInt();
+
+        System.out.println("Dame un tercer número");
+        int numero3 = teclado.nextInt();
+
+        if (numero1==numero2 || numero1==numero3 || numero2==numero3);{
+            System.out.println("ERROR: hay números introducidos que son iguales");
+        }
+    }
+}
