221ac0e65eafed4c41f4127bac805f2707cf6df0
A714\dam1 <manuelpastorx243@gmail.com>
2025-10-17 19:31:57 +0200

    explicación case

---
+++ Ejercicios_1DAM/src/main/java/org/example/Case.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Case {
+    static void main() {
+        int numero = 1;
+        String palabra = "Patricia";
+        char caracter = 'A';
+
+        switch (caracter){
+            case 'x':
+                System.out.println("El alumno es Raul");
+                break;
+            case 'A':
+                System.out.println("La profesora es Patricia");
+                break;
+        }
+    }
+}
