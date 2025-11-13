fd2db5ad42d3621b046d078c6d53e02ad824deae
A714\dam1 <manuelpastorx243@gmail.com>
2025-11-13 18:26:55 +0100

    no message

---
+++ Ejercicios_1DAM/src/main/java/org/example/ActividadEliminarArrays.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class ActividadEliminarArrays {
+    static void main() {
+        int vector[] = {1,2,2,3,4,5,5,6,6};
+
+
+        int tam = 0;
+
+        int aux[] = new int[vector.length];
+
+        for (int i = 0;i<= vector.length-1;i++) {
+
+            if (i == vector.length-1){
+                aux[i] = vector[i];
+                tam++;
+            }else if (vector[i] != vector[i+1]){
+                aux[i] = vector[i];
+                tam++;
+            }
+
+        }
+        System.out.println(Arrays.toString(aux));
+        System.out.println(tam);
+
+        int limpio[] = new int [tam];
+        int j = 0;
+        for (int i = 0; i<aux.length;i++){
+
+            if (aux[i] != 0){
+                limpio[j] = aux[i];
+                j++;
+            }
+        }
+
+        System.out.println(Arrays.toString(limpio));
+    }
+}
--- Ejercicios_1DAM/src/main/java/org/example/EjemploArrays.java
+++ Ejercicios_1DAM/src/main/java/org/example/EjemploArrays.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class EjemploArrays {
     static void main() {
@@ -23,7 +24,13 @@ public class EjemploArrays {
 
         int vector2[] = vector.clone();
         Arrays.sort(vector2);
+        System.out.println(Arrays.toString(vector2));
+        //Arrays.sort(vector2, Collections.reverseOrder());
 
         System.out.println(Arrays.toString(vector2));
+
+        int posicion = Arrays.binarySearch(vector2,5);
+
+        System.out.println(posicion);
     }
 }
