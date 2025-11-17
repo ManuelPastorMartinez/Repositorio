612bd9c68ebf5d441c7908078d54899db4e901ea
A714\dam1 <manuelpastorx243@gmail.com>
2025-11-17 20:25:33 +0100

    no message

--- Ejercicios_1DAM/src/main/java/org/example/Tema3_Practica2.java
+++ Ejercicios_1DAM/src/main/java/org/example/Tema3_Practica2.java
@@ -9,21 +9,26 @@ public class Tema3_Practica2 {
 
         Scanner teclado = new Scanner(System.in);
         Random random = new Random();
-        String numero_propio = teclado.next();
         boolean estado = true;
+        int complementario = 0;
 
-        System.out.println("Introduce los números de tu loteria (formato  N-N-N-N-N-N/R)");
+        System.out.println("Introduce los números de tu loteria (formato N-N-N-N-N-N/R)");
+        String numero_propio = teclado.next();
 
-
-        boolean numero_propio_string = numero_propio.matches("\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d-");
-
+        boolean numero_propio_string = numero_propio.matches("\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}/\\d-");
+        if (numero_propio_string == true){
+            String vector_numero_propio[] = numero_propio.split("[-/]");
+        }else {
+            System.out.println("Este no es un formato válido");
+            return;
+        }
         int numero_ganador[]=new int[6];
         int reintegro[] = new int[1];
         while (estado) {
             for (int i = 0; i < numero_ganador.length; i++) {
                 numero_ganador[i] = random.nextInt(49) + 1;
                 if (i != numero_ganador.length - 1 && numero_ganador[i] == numero_ganador[i + 1]) {
-
+                    numero_ganador[i] = random.nextInt(49) + 1;
                 }
             }
         }
