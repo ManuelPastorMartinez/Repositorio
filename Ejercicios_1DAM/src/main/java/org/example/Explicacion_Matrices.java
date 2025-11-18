2651539ced0e96e33f0a212de393928bd5b62f0e
A714\dam1 <manuelpastorx243@gmail.com>
2025-11-18 17:17:43 +0100

    Explicacion matrices completa

---
+++ Ejercicios_1DAM/src/main/java/org/example/Explicacion_Matrices.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Explicacion_Matrices {
+    static void main() {
+
+        int matriz[][] = {{0,1,2},{3,4,5},{6,7,8}};
+
+        int matriz2[][]= new int[3][3];
+
+        System.out.println(matriz[1][0]);
+        System.out.println(matriz[2][1]);
+
+
+        for (int i=0;i<matriz.length;i++){
+            for (int n=0;n<matriz[0].length;n++){
+
+                System.out.print(matriz[i][n]);
+
+            }
+
+            System.out.println();
+        }
+
+        for (int fila[] : matriz){
+            for (int columna : fila){
+                System.out.print(columna);
+            }
+            System.out.println();
+        }
+
+        for (int fila[] : matriz){
+            System.out.println(Arrays.toString(fila));
+        }
+        int vector[] = {1,2,3,4};
+        for (int num : vector){
+            System.out.println(num);
+        }
+    }
+}
