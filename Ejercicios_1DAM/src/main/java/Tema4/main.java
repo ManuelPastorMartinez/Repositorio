accf7caa8120566d2fa3c524ca98df832e3120fb
A714\dam1 <manuelpastorx243@gmail.com>
2025-12-02 16:10:03 +0100

    Funciones

---
+++ Ejercicios_1DAM/src/main/java/Tema4/main.java
@@ -0,0 +1,16 @@
+package Tema4;
+
+public class main {
+    public static void main(String[] args){
+
+        int numero = funcion();
+        System.out.println(numero);
+
+    }
+
+    public static int funcion(){
+
+        int num = 3;
+        return num;
+    }
+}
