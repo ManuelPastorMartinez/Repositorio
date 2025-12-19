310bfd038f69fd2a4392e5f6e15bb52453290dfc
MANUEL\Usuario <manuelpastormartinezz@gmail.com>
2025-12-19 11:46:40 +0100

    Partido de squash acabado

--- Ejercicios_1DAM/src/main/java/Tema4/PartidoDeSquash_bonito.java
+++ Ejercicios_1DAM/src/main/java/Tema4/PartidoDeSquash_bonito.java
@@ -16,7 +16,8 @@ public class PartidoDeSquash_bonito {
 
         /**
          * Metodo que sirve de contenedor para el programa
-         * Lee el método de puntuación equipos, que sirve para
+         * Lee el método de puntuación equipos, que sirve para controlar la puntuación
+         * El programa sigue hasta que la puntuacion sea "F".
          */
     }
 
@@ -30,6 +31,13 @@ public class PartidoDeSquash_bonito {
 
         }while (!entrada.matches("[ABF]+"));//Mientras que la entrada tenga más letras de las permitidas te seguirá pidiendo la entrada
         return entrada;//Una vez que solo tenga las letras permitidas ya la devuelve
+
+        /**
+         * Pide la entrada del programa.
+         * Pone la entrada en mayusculas para poder controlarla
+         * Controla que solo se puedan las letras A, B o F, si se ponen otras letras, el programa pedirá la entrada de nuevo
+         * @return Puntuación de los equipos validada en mayusculas
+         */
     }
 
 
@@ -42,7 +50,7 @@ public class PartidoDeSquash_bonito {
         String puntuacion = pedirEntrada();//Llamo al metodo pedirEntrada para obtener la puntuación
 
         if (puntuacion.equals("F")) {//Si puntuación es solo F finaliza el programa
-                System.out.print("0-0");
+                System.out.print("Partido terminado");
                 return false;
             }
 
@@ -57,7 +65,12 @@ public class PartidoDeSquash_bonito {
 
                 if (letra == 'F') {//Si en la posicion que esta se encuentra F
                     if (puntuacion_equipoA!=0 || puntuacion_equipoB!=0) {//Y la puntuacion de los 2 equipos es diferente de 0
-                        System.out.print(" "+puntuacion_equipoA + "-" + puntuacion_equipoB);//Imprime la puntuación del set sin salto de línea porque no ha encontrado F aun, por lo que sigue siendo el mismo set
+                        System.out.println(" "+puntuacion_equipoA + "-" + puntuacion_equipoB);//Imprime la puntuación del set sin salto de línea porque no ha encontrado F aun, por lo que sigue siendo el mismo set
+                        if (puntuacion_equipoA>puntuacion_equipoB){
+                            System.out.println("Gana el equipo A");
+                        }else {
+                            System.out.println("Gana el equipo B");
+                        }
                     }
                     break;//Se termina el set al haber encontrado F
                 }
@@ -72,18 +85,31 @@ public class PartidoDeSquash_bonito {
                     }
                     saque = 'B';//El saque pasa a ser del equipo B
                 }
-                if (puntuacion_equipoA>=9 && puntuacion_equipoA-puntuacion_equipoB>1 || puntuacion_equipoB>=9 && puntuacion_equipoB-puntuacion_equipoA>1){
-                    //Si la puntuación de alguno de los 2 equipos llega a nueve y la diferencia de puntos entre los 2 equipos es de 2 o más
-                    System.out.println();//Hace un salto de linea para que no se imprima al lado del anterior resultado
-                    System.out.print(puntuacion_equipoA+"-"+puntuacion_equipoB);//Imprime la puntuación del set
+                if (puntuacion_equipoA>=9 && puntuacion_equipoA-puntuacion_equipoB>1){//Si la puntuación del equipo A llega a 9 y la diferencia entre la puntuacion del equipo A y del equipo B es de 2 puntos o más
+                    System.out.println(puntuacion_equipoA+"-"+puntuacion_equipoB);//Imprime la puntuación del set
+                    System.out.println("Gana el equipo A");
                     //Reinicia la puntuación de los 2 equipos para el siguiente set
                     puntuacion_equipoA=0;
                     puntuacion_equipoB=0;
+                    ;
 
+                } else if (puntuacion_equipoB >= 9 && puntuacion_equipoB - puntuacion_equipoA > 1) {//Si la puntuación del equipo B llega a 9 y la diferencia entre la puntuacion del equipo A y del equipo B es de 2 puntos o más
+                    System.out.println(puntuacion_equipoA+"-"+puntuacion_equipoB);//Imprime la puntuación del set
+                    System.out.println("Gana el equipo B");//Muestra un mensaje
+                    //Reinicia la puntuación de los 2 equipos para el siguiente set
+                    puntuacion_equipoA=0;
+                    puntuacion_equipoB=0;
                 }
             }
             System.out.println();//Hace un salto de linea para no escribir al lado de la puntuación
             return true;//Devuelve verdadero
+
+
+        /**
+         * Va actualizando la puntuación de los 2 equipos y muestra el resultado de cada set cuando este llega a 9 o se encuentra con una F
+         * Cada letra de la entrada es un punto para el equipo que coincide con la letra
+         * El metodo finaliza (devuelve false) cuando no se pone nada o se pone "F"
+         */
     }
 
 }
---
+++ Ejercicios_1DAM/src/test/java/Tema4/PartidoDeSquash_bonitoTest.java
@@ -0,0 +1,21 @@
+package Tema4;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PartidoDeSquash_bonitoTest {
+
+    @Test
+    void pedirEntrada() {
+
+    }
+
+    @Test
+    void puntuacionEquipos() {
+    }
+}
